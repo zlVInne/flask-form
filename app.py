368c486b2f02e27a210a32172bbc5076aa4d6259
zlVInne <viniciosilvanascimento8@gmail.com>
2021-10-20 01:23:54 +0000

    Alteraçoes 1

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+lista = ['T1', 'T2', 'T3']
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', tarefas=lista)
+
+@app.route('/create', methods=['GET'])
+def create():
+    return render_template('create.html')
+
+@app.route('/save', methods=['POST'])
+def save():
+    lista.append(request.form['tarefa'])
+    return render_template('create.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
